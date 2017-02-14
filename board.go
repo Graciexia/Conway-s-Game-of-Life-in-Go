5e4fd70590a3ade136fa17261b7f2b937ff7cce4
xiawei <graciexia@outlook.com>
2017-02-14 16:07:39 -0600

    got an empty board with dead by default

--- board.go
+++ board.go
@@ -1,4 +1,23 @@
 package main
 
+import "fmt"
+
+
+type Board struct {
+	row,column int
+	status [][]int
+}
+
+
+func Initial(row, column int) *Board {
+	s := make([][]int, column)
+	for i := range s {
+		s[i] = make([]int, row)
+	}
+	return &Board{status: s, row: row, column: column}
+}
+
 func main() {
+	l := Initial(4,5)
+	fmt.Print("\x0c", l)
 }
