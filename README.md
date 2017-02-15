c8b5d247f1603cdb8ce8f95ce7e95bc213e64d6f
xiawei <graciexia@outlook.com>
2017-02-15 09:26:51 -0600

    finished coding and add more to readme

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
-# Conway-s-Game-of-Life-in-Go
- Conway's Game of Life in Go for a 48 hours code challenge (starting at 11:30 PM Feb 13rd)
+# Conway's Game of Life in Go Language
+The challenge was to write Conway's Game of Life in one of several languages, include Elm, Rust, Elixir, Clojure, Swift, 
+or Go within a 48 hours. 
+The final solution will be executable in some format and, ideally, will include automated unit tests.
+I started at 11:30 PM Feb 13rd
 
+## Problem Approach
+* analyze the problem to be resolved, as well as constraints (time, tools, etc.)
+* look at different languages and determine which one to use
+* learn about language
+* download development tools (including IDE)
+* set-up development environment and validate correctness
+* research and decide on algorithmic approach
+* write code
+>> determine objects for object oriented design
+>> start creating basic object and methods
+>> test methods as they are developed
+>> build full algorithm, and try to get to minimal viable product
+>> refactor code
+>> build unit tests
 
 ## Algorithm
-When you check each cell's life status, the count of alive includes its self (total count)
-> If total count on the board equals to 3, it is alive;
-> If total count on the board equals to 4, it remains its original life status;
-> otherwise it is dead.
+Upon research, one of the easier algorithms involve counting all the neighbors around a given cell (including itself):
+* If total count equals 3, then the considered cell is alive
+* If total count equals 4, then the considered cell remains unchanged
+* Otherwise, the cell is dead
 
-I have board_a and board_b to flip between each other instead of creating a new board every time.
+Besides that, the board needs two copies at any given time - one for the current state of the board, and one for
+creating the next generation. The avoid the need to create a new board every time, saving memory and avoiding
+unnecessary garbage collection (at the expense of slightly more complicated code).
--- board.go
+++ board.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 
@@ -14,12 +15,20 @@ type Cell struct {
 func (c *Cell) make_alive() {
 	c.status = 1
 }
+
 func (c *Cell) kill() {
 	c.status = 0
 }
+
 func (c *Cell) is_alive() bool {
 	return (c.status == 1)
 }
+
+func (c *Cell) copy_cell_data(from_cell Cell) {
+	c.status = from_cell.status
+}
+
+//print cell format
 func (c *Cell) to_s() string {
 	if c.status == 1 {
 		return "*"
@@ -29,41 +38,89 @@ func (c *Cell) to_s() string {
 
 
 type Board struct {
-	rows    int
-	cols    int
-	cells   [][]Cell
+	rows          int
+	cols          int
+	cells_current [][]Cell
+	cells_copy    [][]Cell
 }
+
 func (b *Board) initialize(rows, columns int) {
 	b.rows = rows
 	b.cols = columns
-	b.cells = make([][]Cell, b.rows)
-	for i := range b.cells {
-		b.cells[i] = make([]Cell, b.cols)
+	b.cells_current = make([][]Cell, b.rows)
+	b.cells_copy = make([][]Cell, b.rows)
+	for i := range b.cells_current {
+		b.cells_current[i] = make([]Cell, b.cols)
+		b.cells_copy[i] = make([]Cell, b.cols)
 	}
-	for _, row := range b.cells {
-		for col_ind := range row {
-			//row[col_ind].kill()
-			row[col_ind].make_alive()
+	for row_index := range b.cells_current {
+		for col_index := range b.cells_current[row_index] {
+			b.cells_current[row_index][col_index].kill()
+			b.cells_copy[row_index][col_index].kill()
 		}
 	}
 }
+
+func (b *Board) make_cell_alive(row_index, col_index int) {
+	b.cells_current[row_index][col_index].make_alive()
+}
+
+func (b *Board) kill_cell(row_index, col_index int) {
+	b.cells_current[row_index][col_index].kill()
+}
+
+// always checks the copy
 func (b *Board) live_sum(row_index, col_index int) int {
 	count := 0
 	start_row := row_index - 1; if start_row < 0 { start_row = 0 }
 	end_row := row_index + 2; if end_row > b.rows { end_row = b.rows }
 	start_col := col_index - 1; if start_col < 0 { start_col = 0 }
 	end_col := col_index + 2; if end_col > b.cols { end_col = b.cols }
-	for _, row := range b.cells[start_row:end_row] {
+	for _, row := range b.cells_copy[start_row:end_row] {
 		for _, cell := range row[start_col:end_col] {
 			if cell.is_alive() { count += 1 }
 		}
 	}
 	return count
 }
+
+// checks the copy but sets the current cells
+func (b *Board) make_live_or_kill(row_index, col_index int) {
+	total_live := b.live_sum(row_index, col_index)
+	if total_live == 3 {
+		b.make_cell_alive(row_index, col_index)
+	} else if total_live != 4 {
+		b.kill_cell(row_index, col_index)
+	}
+}
+
+func (b *Board) copy_cells() {
+	for row_index := range b.cells_current {
+		for col_index := range b.cells_current[row_index] {
+			b.cells_copy[row_index][col_index].copy_cell_data(b.cells_current[row_index][col_index])
+		}
+	}
+}
+
+func (b *Board) change_life() {
+	b.copy_cells()
+	for row_index := range b.cells_copy {
+		for col_index := range b.cells_copy[row_index] {
+			b.make_live_or_kill(row_index, col_index)
+		}
+	}
+}
+
+func (b *Board) set_living_cells(cell_coords [][]int) {
+	for _, coord := range cell_coords {
+		b.make_cell_alive(coord[0], coord[1])
+	}
+}
+
 func (b *Board) print_board() {
 	clear_screen()
 	fmt.Println("+-" + strings.Repeat("--", b.cols) + "+")
-	for _, row := range b.cells {
+	for _, row := range b.cells_current {
 		fmt.Print("| ")
 		for _, cell := range row {
 			fmt.Print(cell.to_s() + " ")
@@ -71,11 +128,6 @@ func (b *Board) print_board() {
 		fmt.Println("|")
 	}
 	fmt.Println("+-" + strings.Repeat("--", b.cols) + "+")
-}
-
-func (b *Board) live_or_die(row_index, col_index int) {
-	//total_lilve = func field_sum(board, row_index, col_index)
-
 }
 
 func clear_screen() {
@@ -86,9 +138,22 @@ func clear_screen() {
 
 func main() {
 	var board Board
-	board.initialize(10,10)
-	clear_screen()
+	board.initialize(10,16)
+	// set up initial board configuration
+	// Pentadecathlon
+	board.set_living_cells([][]int{{3,5}, {3,10}, {4,3}, {4,4}, {4,6}, {4,7}, {4,8}, {4,9}, {4,11}, {4,12}, {5,5}, {5,10}})
+	// Glider
+	//board.set_living_cells([][]int{{2,1}, {2,2}, {2,3}, {1,3}, {0,2}})
 	board.print_board()
-	fmt.Println(board.live_sum(3,3))
+	for i := 0; i < 100; i++ {
+		time.Sleep(10 * time.Millisecond)
+		board.change_life()
+		board.print_board()
+	}
 }
+
+
+
+
+
 
